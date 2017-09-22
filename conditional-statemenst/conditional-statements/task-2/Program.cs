5566323cba5f06305eac83fa4aa40769b45ee765
SannaNaTu <korhonsm@gmail.com>
2017-09-22 13:37:35 +0300

    task2

---
+++ conditional-statemenst/conditional-statements/task-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma selvittaa onko syotetty luku pariton tai parillinen. ");
+            bool isNumber;
+          
+           
+
+                Console.Write("Syota luku: ");
+                // Define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int evaluetedNumber;
+                isNumber = int.TryParse(userInput, out evaluetedNumber);
+
+            // program logic
+            if(evaluetedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syotit luvun {evaluetedNumber}, se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Syotit luvun {evaluetedNumber}, se on pariton");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ conditional-statemenst/conditional-statements/task-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla. ");
+            bool isNumber;
+            do
+            {
+
+                Console.Write("Syota luku: ");
+                // Define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int evaluetedNumber;
+                isNumber = int.TryParse(userInput, out evaluetedNumber);
+
+                // program logic
+                if (isNumber == true)
+                {
+                    if (evaluetedNumber < 0)
+                    {
+                        Console.WriteLine($"Numero {evaluetedNumber} on negatiivinen");
+                    }
+                    else if (evaluetedNumber > 0)
+                    {
+                        Console.WriteLine($"Numero {evaluetedNumber} on positiivinen");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluetedNumber} on nolla");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syotit muuta kuin numeroita!");
+                }
+            } while (isNumber == false);
+
+
+            Console.ReadKey();
+        }
+    }
+}
