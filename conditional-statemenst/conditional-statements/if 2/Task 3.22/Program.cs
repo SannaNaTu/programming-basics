a4ab0277b6487749b15480b25a96780a2d499044
SannaNaTu <korhonsm@gmail.com>
2017-10-22 18:31:34 +0300

    new way for task 3.2

---
+++ conditional-statemenst/conditional-statements/if 2/Task 3.22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_3._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna luku ykkösestä ylöspäin. Ohjelma laskee yhteen antamasi numeron + yhdeksän sitä edeltänyttä lukua");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            if (number < 1)
+            {
+                Console.WriteLine("Annoit liian pienen arvon.");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit: { number} \nVastaus: {number + (number - 1) + (number - 2) + (number - 3) + (number - 4) + (number - 5) + (number - 6) + (number - 7 )+ (number - 8 )+ (number - 9) } ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
