8c9a8f83d25b5b17099e8fdfac9524f7a6cf76c8
SannaNaTu <korhonsm@gmail.com>
2017-10-22 18:04:25 +0300

    Empty lines taken off

--- conditional-statemenst/conditional-statements/if 2/ConsoleApp2/Program.cs
+++ conditional-statemenst/conditional-statements/if 2/ConsoleApp2/Program.cs
@@ -15,24 +15,13 @@ namespace ConsoleApp2
             if (number < 0)
             {
                 Console.WriteLine("Virheellinen syöte");
-                //    Console.ReadLine();
-                //    return;
-                // nämä voisi olla alla olevan elsen tilalla
             }
             else
             { 
-            //do // tämä on vaihtoehto for lausekkeelle
-            //{
-            //    i = i + 1;
-            //    f = f * i;
-            //} while (i < number);
-
-             for (i=1;i<=number;i++)
+            for (i=1;i<=number;i++)
                 {
                     f = f * i;
                     Console.WriteLine($"{i} !={ f} "); // esim. 5! =120
-
-
                 }
             Console.WriteLine($"Syötit: { number} \nVastaus: { f} ");
             }
