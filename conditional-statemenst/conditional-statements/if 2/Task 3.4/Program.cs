48342ee35f1ec967a46ac432948f25228f2aeadb
SannaNaTu <korhonsm@gmail.com>
2017-10-23 12:09:11 +0300

    task 3.4 and 3.5

---
+++ conditional-statemenst/conditional-statements/if 2/Task 3.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna luku ykkösestä ylöspäin. Ohjelma laskee yhteen antamasi arvon sekä sitä edeltäneet arvot. ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int sum = 0;
+            int i = 0;
+            if (number < 1)
+            {
+                Console.WriteLine("Annoit liian pienen arvon.");
+            }
+            else
+            {
+                do
+                {
+                    i++;
+                    sum = sum + i;
+                } while (i < number);
+                Console.WriteLine($"vastaus: {sum}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
