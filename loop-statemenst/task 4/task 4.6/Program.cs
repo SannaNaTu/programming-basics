59bc1c31400fdaa9a225bec4c47cac7a84a0d1c1
SannaNaTu <korhonsm@gmail.com>
2017-10-24 13:35:58 +0300

    task 4.6 update

--- loop-statemenst/task 4/task 4.6/Program.cs
+++ loop-statemenst/task 4/task 4.6/Program.cs
@@ -6,7 +6,20 @@ namespace task_4._6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma heittää 1000 kertaa noppaa puolestasi ja kertoo kuinka moni niistä oli kuutonen.");
+            Random rnd = new Random();
+            int sixCounter = 0;
+
+            for (int number = 0; number < 1000; number++)
+            {
+                int rndNumber = rnd.Next(1, 7);
+                Console.WriteLine($"{number + 1}. {rndNumber}");
+                if (rndNumber == 6)
+                    sixCounter++;
+            }
+            Console.WriteLine($"Kutosia esiintyi: {sixCounter}kpl");
+            Console.ReadKey();
         }
     }
 }
