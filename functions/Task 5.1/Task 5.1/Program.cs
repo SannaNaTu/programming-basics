816bb3707c25e459bf001075997bfcda30bbfdb2
SannaNaTu <korhonsm@gmail.com>
2017-10-24 13:46:54 +0300

    task 5.1

---
+++ functions/Task 5.1/Task 5.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
