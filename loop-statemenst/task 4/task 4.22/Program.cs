cb7d67609c3ccce053b87b3d21251fbbeb030be2
SannaNaTu <korhonsm@gmail.com>
2017-10-24 10:00:51 +0300

    task 4.2

---
+++ loop-statemenst/task 4/task 4.22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task_4._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9 ");
+            for (int i = 1; i < 10; i++)
+            for (int j = 1; j < 10; j++)
+            {
+                Console.WriteLine($"{i} x {j} = {i*j}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
