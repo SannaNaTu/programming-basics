79b72d8a28f7e06bac231027271abf937b92cfe7
SannaNaTu <korhonsm@gmail.com>
2017-10-25 11:04:40 +0300

    task 5.2

---
+++ functions/Task 5.1/5.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna ohjelmalle kaksi lukua. Ohjelma kertoo kumpi luvuista on pienempi ja kumpi suurempi");
+            int x = 0;
+            int y = 0;
+            Console.Write("Anna luku yksi: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku kaksi: ");
+            y = int.Parse(Console.ReadLine());
+            if (x < y)
+            {
+                Console.WriteLine($"Luku {y} on isompi kuin luku {x}");
+            }
+            else
+            {
+                Console.WriteLine($"Luku {x} on isompi kuin luku {y}");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
