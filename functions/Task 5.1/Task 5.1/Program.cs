c04374815ef73fb7f3ce2988cb41f8a87f920607
SannaNaTu <korhonsm@gmail.com>
2017-10-25 09:56:17 +0300

    task 5.1

--- functions/Task 5.1/Task 5.1/Program.cs
+++ functions/Task 5.1/Task 5.1/Program.cs
@@ -7,6 +7,37 @@ namespace Task_5._1
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-        }
+            
+                Console.WriteLine("Anna ohjelmalle luku. Ohjelma tulostaa näytölle luvun verran tähtiä");
+
+                int s;
+                while (true)
+                {
+                    Console.Write("Tähtien lukumäärä: ");
+                    s = int.Parse(Console.ReadLine());
+                    if (s < 0)
+                    {
+                        Console.WriteLine("Luku {0} ei ole sallittu luku", s);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                string asterisks = getNAsterisks(s);
+                Console.WriteLine(asterisks);
+                Console.ReadKey();
+            }
+
+            static string getNAsterisks(int s)
+            {
+                string res = string.Empty;
+                for (int i = 0; i < s; i++)
+                {
+                    res += '*';
+                }
+                return res;
+            }
+        
     }
 }
