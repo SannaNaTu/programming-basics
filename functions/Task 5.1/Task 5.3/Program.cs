c1576d7554aa9566ddd4dbf2096e94dd7434a005
SannaNaTu <korhonsm@gmail.com>
2017-10-25 14:06:58 +0300

    Task 5.3

--- functions/Task 5.1/Task 5.3/Program.cs
+++ functions/Task 5.1/Task 5.3/Program.cs
@@ -7,35 +7,42 @@ namespace Task_5._3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Anna ohjelmalle luku väliltä 20-30");
-            int x = 0;
-            bool userInput = int.TryParse(Console.ReadLine(), out x);
-            if (userInput == true)
-            {
-                Console.WriteLine($"{minimum(x)}");
-            }
-            else
-            {
-                Console.WriteLine("Luku ei ollut annetulta väliltä tai se ei ollut numero");
-            }
+
+            Console.WriteLine($"OK {NumberFromRange(-2, 10)}");
+
             Console.ReadKey();
-
         }
-        static string minimum(int x)
+
+        static int NumberFromRange(int lowerBound, int upperBound)
         {
-            string answer;
+            string userInput = string.Empty;
+            bool isNumber = false;
+            int x = 0;
+            int retNumber = 0;
+            bool isValidNumber = false;
 
-            if (x < 30+1 && x > 20-1)
+            do
             {
-                answer = $"Luku {x} on hyväksytty";
-            }
+                Console.WriteLine($"Anna ohjelmalle luku {lowerBound} ja {upperBound} väliltä");
+                userInput = Console.ReadLine();
+                isNumber = int.TryParse(userInput, out x);
+                if (isNumber)
+                    if (x > lowerBound && x < upperBound)
+                    {
+                        retNumber = x;
+                        isValidNumber = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Luku {x} ei ollut annetulta väliltä");
+                    }
+                else
+                    Console.WriteLine($"{userInput.ToUpper()} ei ole luku! ");
 
-            else
-            {
-                answer = $"Luku ei ollut annetulta väliltä tai se ei ollut numero";
-            }
+            } while ((!isNumber || !isValidNumber));
 
-            return answer;
+
+            return retNumber;
         }
     }
 }
