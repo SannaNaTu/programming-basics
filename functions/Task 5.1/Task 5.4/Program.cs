64024ae5abbc3224796d5c909334d4a78c547aef
SannaNaTu <korhonsm@gmail.com>
2017-10-26 18:36:20 +0300

    task 5.4

---
+++ functions/Task 5.1/Task 5.4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Suurin syöttämäsi luku oli " + Numero());
+            Console.ReadKey();
+            }
+            static int Numero()
+            {
+                int num = 0;
+                int max = 0;
+                bool isNumber;
+                Console.WriteLine("Anna 10 positiivista lukua");
+            //Countteri
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"{i + 1}\t= ");
+
+                    if (isNumber = int.TryParse(Console.ReadLine(), out num) && num > 0)
+                    {
+                        if (max < num)
+                    {
+                        max = num;
+                    }
+                }
+                    else
+                    {
+                        Console.WriteLine("Väärä syöte, anna positiivinen luku");
+                        i--;
+                    }
+                }
+                return max;
+            }
+        }
+}
