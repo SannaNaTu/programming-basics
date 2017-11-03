83f113a88df078b335af7bc18861d955cb4163a9
SannaNaTu <korhonsm@gmail.com>
2017-11-03 13:29:16 +0200

    test 2

---
+++ learning test/test 1/test 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna ohjelmalle sana tai lause!");
+            string userInput;
+            userInput = Console.ReadLine();
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"{i} {userInput}");
+                Console.ReadKey();
+        }
+    }
+}
---
+++ learning test/test 1/test 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna ohjelmalle sana tai lause!Ohjelma tulostaa yhtä monta riviä kuin kirjaimia on.");
+            string userInput = Console.ReadLine();
+            string input = userInput.Replace(" ", "");
+            for (int i = 0; i < input.Length; i++)
+            Console.WriteLine($"{i+1}. {userInput}");
+            Console.ReadKey();
+        }
+    }
+}
