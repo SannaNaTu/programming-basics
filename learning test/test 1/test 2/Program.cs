7e28969c1a1db492c3b51b475ceba0dcaaf84f22
SannaNaTu <korhonsm@gmail.com>
2017-11-03 14:15:51 +0200

    fixes

--- learning test/test 1/test 2/Program.cs
+++ learning test/test 1/test 2/Program.cs
@@ -7,7 +7,7 @@ namespace test_2
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Anna ohjelmalle sana tai lause!Ohjelma tulostaa yhtä monta riviä kuin kirjaimia on.");
+            Console.WriteLine("Anna ohjelmalle sana tai lause!Ohjelma tulostaa yhtä monta kertaa lauseen kuin kirjaimia on.");
             string userInput = Console.ReadLine();
             string input = userInput.Replace(" ", "");
             for (int i = 0; i < input.Length; i++)
