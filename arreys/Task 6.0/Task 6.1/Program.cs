5249b8a98bf18ea482a8964c063d40d0832707ce
SannaNaTu <korhonsm@gmail.com>
2017-11-03 16:26:10 +0200

    lotto stuff

---
+++ arreys/Task 6.0/Task 6.1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Lottokone:");
+            Random rnd = new Random();
+            int i = 0;
+            int[] lotto = new int[40];
+            int addnumber; //kumpi?
+            int doublenumber;
+
+                while (i < 7)
+                {
+                    int rndIndex = rnd.Next(0, 39 + 1);
+                    if (lotto[rndIndex] == 0)
+                    {
+                        lotto[rndIndex] = 1;
+                        i++;
+                    }
+                }
+                while (true)
+                {
+                    int rndIndex = rnd.Next(0, 39 + 1);
+                    if (lotto[rndIndex] == 0)
+                    {
+                        lotto[rndIndex] = 2;
+                        break;
+                    }
+                }
+          
+                string lottonumbers = "";
+            
+                for (i = 0; i < lotto.Length; i++) // tarviiko intin
+                    if (lotto[i] == 1)
+                        lottonumbers += $"{i + 1} ";
+            //int addnumber;
+            for ( i = 0; i < lotto.Length; i++) // tarviiko intin
+                    if (lotto[i] == 2)
+                    {
+                        addnumber = i + 1;
+                    break;
+                    }
+            for (i = 0; i < lotto.Length; i++)
+            {
+                {
+                    doublenumber = rnd.Next(0, 39 + 1);
+                }
+                {
+                    Console.WriteLine($"Lottonumerosi ovat:{lottonumbers} lisänumero:{addnumber} ja tuplausnumero:{doublenumber}");
+                }
+
+                // Lotto arvonta
+                //Lisänumeron arvonta
+                //Lottonron tulostus
+                //Lisänron tulostus
+                //Tuplausnro
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
