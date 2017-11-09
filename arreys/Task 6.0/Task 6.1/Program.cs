0b50d07534f2705895809acee875aa80a5ced01d
SannaNaTu <korhonsm@gmail.com>
2017-11-09 08:35:29 +0200

    task 6.1 fixes

--- arreys/Task 6.0/Task 6.1/Program.cs
+++ arreys/Task 6.0/Task 6.1/Program.cs
@@ -11,8 +11,8 @@ namespace Task_6._1
             Random rnd = new Random();
             int i = 0;
             int[] lotto = new int[40];
-            //int addnumber; //kumpi?
             int doublenumber;
+            // Arvotaan oikea lottorivi
             while (i < 7)
             {
                 int rndIndex = rnd.Next(0, 39 + 1);
@@ -22,6 +22,7 @@ namespace Task_6._1
                     i++;
                 }
             }
+            //Arvotaan lisänro
             while (true)
             {
                 int rndIndex = rnd.Next(0, 39 + 1);
@@ -31,33 +32,28 @@ namespace Task_6._1
                     break;
                 }
             }
+            //Tulostus lottonumeroille
             string lottonumbers = "";
 
             for (i = 0; i < lotto.Length; i++)
+            {
                 if (lotto[i] == 1)
                     lottonumbers += $"{i + 1} ";
+            }
             int extranumber = 0;
             for (i = 0; i < lotto.Length; i++)
+            {
                 if (lotto[i] == 2)
                 {
                     extranumber = i + 1;
                     break;
                 }
-            for (i = 0; i < lotto.Length; i++)
-            {
-                {
-                    doublenumber = rnd.Next(0, 39 + 1);
-                }
-                {
-                    Console.WriteLine($"Lottonumerosi ovat:{lottonumbers} lisänumero:{extranumber} ja tuplausnumero:{doublenumber}");
-                }
-                // Lotto arvonta
-                //Lisänumeron arvonta
-                //Lottonron tulostus
-                //Lisänron tulostus
-                //Tuplausnro
-                Console.ReadKey();
             }
+
+            doublenumber = rnd.Next(0, 39 + 1);
+
+            Console.WriteLine($"Lottonumerosi ovat:{lottonumbers} lisänumero:{extranumber} ja tuplausnumero:{doublenumber}");
+            Console.ReadKey();
 
         }
     }
