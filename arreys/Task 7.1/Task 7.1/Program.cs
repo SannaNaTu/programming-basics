d84893b72b1b7f0f0759d94b1e46b4fe272bdf9d
SannaNaTu <korhonsm@gmail.com>
2017-11-18 02:17:16 +0200

    final fixes

--- arreys/Task 7.1/Task 7.1/Program.cs
+++ arreys/Task 7.1/Task 7.1/Program.cs
@@ -4,24 +4,27 @@ namespace Task_7._1
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             string[] numbers = new string[10];
 
+
             Console.WriteLine("[X] = Arvo");
 
             for (int i = 0; i < 10; i++)
             {
-                 numbers[i] = getNum();
-                Console.WriteLine($"[{i}] = {numbers[i]} "); // random number saatava tänne numbers[i]
+
+                    numbers[i] = getNum();
+                    Console.WriteLine($"[{i}] = {numbers[i]} ");
+                
             }
 
             Console.ReadKey();
         }
         static string getNum()
         {
-            Random rnd = new Random();
             int N = rnd.Next(1, 20+1);
             string num = "";
 
--- learning test/extra task 1/extra task 2/Program.cs
+++ learning test/extra task 1/extra task 2/Program.cs
@@ -9,36 +9,40 @@ namespace extra_task_2
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Minäpä tiedän luvun väliltä 1-100, jota sinä et tiedä!");
             Random rnd = new Random();
-            int UserInput = 0;
-            int number = rnd.Next(10);
-            int sum = 0;
-            while (number != UserInput)
-
+            int j = rnd.Next(0,100+1);
+            int guess = 0;
+            int i;
+            do
             {
                 Console.Write("Arvaa luku:");
-                int.TryParse(Console.ReadLine(), out UserInput);
-                if (UserInput < number)
+                string userInput = Console.ReadLine();
+                i = int.Parse(userInput);
+                guess++;
+                if (j == i)
                 {
-                    Console.WriteLine("Lukuni on isompi!");
+                    Console.WriteLine($"Oikein! Arvauksia yhteensä: {guess} ");
                 }
-                else if (UserInput > number)
+                else if (j < i)
                 {
-
                     Console.WriteLine("Lukuni on pienempi!");
                 }
-                else
+                else if (j > i)
                 {
-                    //sum = UserInput;
-                    UserInput++;
-                    Console.WriteLine($"Oikein! Arvauksia yhteensä: {UserInput} ");
-                    break;
+
+                    Console.WriteLine("Lukuni on suurempi!");
+                }
+                if (i > 100 || i < 1)
+                {
+                    Console.WriteLine("Numerosi ei myöskään ole sallitulla alueella!");
                 }
             }
+            while (j != i);
 
-            Console.ReadKey();
+                Console.ReadKey();
+            }
         }
     }
-}
+
 
            
         
