91e28bb83785413ba407496cfb7c1b88fbd6dd58
SannaNaTu <korhonsm@gmail.com>
2017-11-29 09:23:40 +0200

    Task 5

---
+++ string-handling/Task 1/Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int count= 0;
+            Console.WriteLine("Ohjelma kertoo kuinka monta vokaalia lauseessa on!");
+            Console.Write("Anna lause:");
+            string UserInput = Console.ReadLine().ToUpper();
+
+               foreach (char letter in UserInput)
+                {
+                if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'Y' || letter == 'Ä' || letter == 'Ö')
+                    count++;
+                    }
+                Console.WriteLine($"Vokaaleita on: {count}");
+            Console.ReadLine();
+        }
+    }
+}
