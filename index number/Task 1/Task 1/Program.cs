4049ef3f22aa07b550d229a6bf45836e1597a112
SannaNaTu <korhonsm@gmail.com>
2017-12-06 19:48:37 +0200

    reorganised

--- index number/Task 1/Task 1/Program.cs
+++ index number/Task 1/Task 1/Program.cs
@@ -8,9 +8,31 @@ namespace Task_1
         {
             Console.WriteLine("Ohjelma tarkistaa viitenumeron!");
             Console.Write("Anna numero:");
-            string UserInput = Console.ReadLine().Replace(" ", "").Replace("-", "");
 
+            string userInput = Console.ReadLine().Replace(" ", "").Replace("-", "");
+            string ch = userInput.Substring(userInput.Length - 1, 1);
+            string refnumber = userInput.Substring(0, userInput.Length - 1);
+
+            int[] k = {7,3,1};
+            int result = 0;
+            for(int i=0; i<refnumber.Length;i++)
+            {
+                int j = refnumber.Length - 1 - i;
+                int number = int.Parse(refnumber[j].ToString());
+                result += number * k[i % 3];
+            }
+            int check = (result / 10 + 1) * 10 - result;
+
+            if (check == int.Parse(userInput[userInput.Length - 1].ToString()))
+            {
+                Console.WriteLine("Viitenumero on kunnossa!");
+            }
+            else
+            {
+                Console.WriteLine("Viitenumero on virheellinen!");
+            }
+            Console.ReadKey();
         }
-        Console.ReadLine();
+        
     }
 }
---
+++ my program/Black or White game/Black or White game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Black_or_White_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Peli arpoo punaisen tai mustan kortin. Koeta arvata kumpi väri kortissa on!");
+            Console.Write(":");
+        }
+    }
+}
