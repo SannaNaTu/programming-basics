aa33e651c5cc2d15452a5e6957f3c7e555b3976b
SannaNaTu <korhonsm@gmail.com>
2017-12-15 15:30:34 +0200

    minor fixes

--- arreys/Array 3/Task 7.3/Program.cs
+++ arreys/Array 3/Task 7.3/Program.cs
@@ -7,46 +7,94 @@ namespace Task_7._3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.WriteLine("Ohjelma määrittelee kolme taulukkoa, joissa on jokaisessa 10 lukua.");
-
+            Console.WriteLine("Ohjelma antaa kolme taulukkoa, joissa jokaisessa on 10 satunnaista lukua.");
+            Console.WriteLine();
+            Random rnd = new Random();
             int[] arrT_1 = new int[10];
             int[] arrT_2 = new int[10];
             int[] arrT_3 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arrT_1[i] = rnd.Next(51);
+                arrT_2[i] = rnd.Next(51);
 
-            Random rnd = new Random();
-
-            for (int i = 0; i < 10; i++)
-            {
-                arrT_1[i] = rnd.Next(0, 10);
-                //Console.Write($"{i + 1} {Printer(arrT_1[i])}");
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                arrT_2[i] = rnd.Next(0, 10);
-                //Console.Write($"{Printer(arrT_2[i])} ");
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                if (arrT_1[i] < arrT_2[i])
-                {
-                    arrT_3[i] = arrT_2[i];
-                }
-                else
+                if (arrT_1[i] > arrT_2[i])
                 {
                     arrT_3[i] = arrT_1[i];
                 }
+                else
+                {
+                    arrT_3[i] = arrT_2[i];
+                }
+            }
+            Console.Write("        ");
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 10)
+                {
+                    Console.Write($"0{i} ");
+                }
+                else
+                {
+                    Console.Write($"{i}\n");
+                }
             }
 
+            Console.Write("        ");
+            for (int i = 0; i < 29; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"arrT_1: {Numbers(arrT_1[i])} ");
+                }
+                else
+                {
+                    Console.Write($"{Numbers(arrT_1[i])} ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"arrT_2: {Numbers(arrT_2[i])} ");
+                }
+                else
+                {
+                    Console.Write($"{Numbers(arrT_2[i])} ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"arrT_3: {Numbers(arrT_3[i])} ");
+                }
+                else
+                {
+                    Console.Write($"{Numbers(arrT_3[i])} ");
+                }
+            }
 
             Console.ReadKey();
         }
-        static string Printer(int[] arrX)
+        static string Numbers(int x)
         {
-            string res = "";
-
-
-            return res;
-
+            if (x < 10)
+            {
+                return "0" + x.ToString();
+            }
+            else
+            {
+                return x.ToString();
+            }
         }
     }
 }
+
