62155d274763e6c96a162faa1386368c2e55d8d0
SannaNaTu <korhonsm@gmail.com>
2017-12-15 15:08:21 +0200

    some minor fixes

--- id/id/id/Program.cs
+++ id/id/id/Program.cs
@@ -6,7 +6,54 @@ namespace id
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelmaa kertoo onko henkilötunnus oikea!");
+            Console.WriteLine("Anna ohjelmalle henkilötunnus:");
+            {
+                string userInput = Console.ReadLine().Replace(" ", "").ToUpper();
+
+                userInput = Remove(userInput.lenght - 1, 1).userInput.Replace("+", "").userInput.Replace("-", "").userInput.Replace("A", "");
+
+                //int num = int.Parse(userInput);
+                string idcheck = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+                int 
+
+                if
+                {
+                    Console.WriteLine("Henkilötunnus on OK");
+                }
+                else
+                {
+                    Console.WriteLine("Henkilötunnus on väärin"); 
+                }
+
+
+
+
+
+                //string idcheck = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+
+
+
+
+
+
+
+                //    if ((userInput.Length == 11) || (userInput.Substring(6, 7) != "-") && (userInput.Substring(6, 7) != "+") && (userInput.Substring(6, 7) != "A"))
+                //    {
+                //        Console.WriteLine("");
+                //        Console.ReadLine();
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("");
+                //    }
+                //    return realid.ToUpper();
+
+                Console.ReadKey();
+                //}
+
+            }
         }
     }
 }
--- learning test/test 1/Test 3/Program.cs
+++ learning test/test 1/Test 3/Program.cs
@@ -7,6 +7,26 @@ namespace Test_3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Anna ohjelmalle lukuja. Ohjelma laskee lopulta kaikki antamasi numerot yhteen kun annat -1.");
+            int summa = 0;
+            int userInput = 0;
+            bool isNumber = true;
+            while (isNumber)
+            {
+                Console.Write("Anna luku: ");
+                isNumber = int.TryParse(Console.ReadLine(), out userInput);
+
+                if (userInput == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    summa += userInput;
+                }
+            }
+            Console.WriteLine($"Annoit -1 , joten lukujen summa on: {summa}");
+            Console.ReadKey();
         }
     }
 }
